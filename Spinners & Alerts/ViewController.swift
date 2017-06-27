487f7d0d6f51278eff027fcb0200aca8b229808c
Tyler McGee <tmcgee2481@gmail.com>
2017-06-27 08:57:53 -0400

    Committed 6/27/17.

--- Spinners & Alerts/ViewController.swift
+++ Spinners & Alerts/ViewController.swift
@@ -9,7 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activityIndicator = UIActivityIndicatorView()
 
+    @IBAction func createAlert(_ sender: Any) {
+        
+      let alertController = UIAlertController(title: "Hey There!", message: "Are you sure?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            
+            print("button pressed")
+            
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "No", style: .default, handler: { (action) in
+            
+            print("button pressed")
+            
+            self.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func pauseApp(_ sender: Any) {
+        
+        activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        activityIndicator.center = self.view.center
+        
+        activityIndicator.hidesWhenStopped = true
+        
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+        
+        //UIApplication.shared.beginIgnoringInteractionEvents()
+        
+    }
+    
+    @IBAction func restoreApp(_ sender: Any) {
+        
+        activityIndicator.stopAnimating()
+        
+        //UIApplicaton.shared.endIgnoringInteractionEvents()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
